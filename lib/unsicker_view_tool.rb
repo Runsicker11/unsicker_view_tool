49ee299cc952689e19e0eb1eab3ffa0072d2d083
Jordan Hudgens <jordan@devcamp.com>
2017-06-26 18:17:13 +0000

    Dev Tool initial commit

---
+++ lib/unsicker_view_tool.rb
@@ -0,0 +1,6 @@
+require "unsicker_view_tool/version"
+require "unsicker_view_tool/renderer"
+
+module UnsickerViewTool
+  # Your code goes here...
+end
---
+++ lib/unsicker_view_tool/version.rb
@@ -0,0 +1,3 @@
+module UnsickerViewTool
+  VERSION = "0.1.0"
+end
